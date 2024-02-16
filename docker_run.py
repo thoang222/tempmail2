cd530ae7417a8faff3118abdd5fc477227943821
thoang222 <156932337+thoang222@users.noreply.github.com>
2024-02-17 06:26:44 +0700

    Update docker_run.py

--- docker_run.py
+++ docker_run.py
@@ -12,15 +12,23 @@ def build_image_from_dockerfile():
 
 def run_container(image, port):
     client = docker.from_env()
+    environment = {'PORT': port} 
     ports = {'{0}/tcp'.format(port): port}
-    container = client.containers.run(image, ports=ports, detach=True)
-    print(f'Container {container.id} is running on port {port}')
+    container = client.containers.run(
+        image,
+        ports=ports,
+        detach=True,
+        environment=environment,
+        restart_policy={'Name': 'always'} 
+    )
+    print(f'Container {container.id} is running on port {port} with restart policy set to "always"')
+
 
 if __name__ == "__main__":
     docker_image = build_image_from_dockerfile()
 
     if docker_image:
-        for port in range(5000, 5021):
+        for port in range(5000, 5006):
             run_container(docker_image.tags[0], port)
     else:
         print("Failed to build Docker image.")
